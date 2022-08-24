bf070f930a56ca689f64dada8db5a95500bb5cad
Jakushin Cho <weezer970@gmail.com>
2022-08-24 10:24:55 +0900

    ToDoアプリ練習

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>ToDo App</h1>
+  <input type="text" v-model="newTodoText" /><button @click="addTodo">追加</button><button @click="clearDoneTodos">完了済みを削除する</button>
+  <ul>
+    <p v-if="todos.length === 0">ToDoがありません</p>
+    <li v-for="todo in todos" v-bind:key="todo.id">
+      <input type="checkbox" v-model="todo.isDone" /><span :class="{ 'todo-done': todo.isDone }">{{ todo.text }}</span>
+    </li>
+  </ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      newTodoText: '',
+      todos: []
+    }
+  },
+  methods: {
+    addTodo() {
+      if(this.newTodoText === '') {
+        alert('文字を入力してください')
+        return;
+      }
+      this.todos.push({
+        isDone: false,
+        text: this.newTodoText
+      })
+      this.newTodoText = ''
+    },
+    clearDoneTodos() {
+      this.todos = this.todos.filter((todo) => !todo.isDone)
+    },
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background-color: #eee;
+}
+
+.todo-done {
+  text-decoration: line-through;
 }
 </style>
